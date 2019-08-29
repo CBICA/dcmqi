87ceed3fdc9ed5791528ddb66491420018c65b27
Sarthak Pati <sarthak.pati@uphs.upenn.edu>
2019-08-29 10:26:36 -0400

    getting this to compile properly

--- include/dcmqi/QIICRConstants.h
+++ include/dcmqi/QIICRConstants.h
@@ -2,11 +2,11 @@
 #define DCMQI_QIICRCONSTANTS_H
 
 // DCMQI includes
-#include "dcmqi/internal/VersionConfigure.h"
+//#include "dcmqi/internal/VersionConfigure.h"
 
-#define QIICR_MANUFACTURER "QIICR"
+#define QIICR_MANUFACTURER "CaPTk by CBICA @ UPenn"
 #define QIICR_DEVICE_SERIAL_NUMBER "0"
-#define QIICR_MANUFACTURER_MODEL_NAME dcmqi_WC_URL
-#define QIICR_SOFTWARE_VERSIONS dcmqi_WC_REVISION
+#define QIICR_MANUFACTURER_MODEL_NAME "http://www.cbica.upenn.edu/captk"
+#define QIICR_SOFTWARE_VERSIONS "1.0.0"
 
 #endif //DCMQI_QIICRCONSTANTS_H
